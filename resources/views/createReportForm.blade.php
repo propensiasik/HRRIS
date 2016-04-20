e77834e248e2597540c459b2a5cfbf0110d9abf2
khalilahunafa <khalila9616@gmail.com>
2016-04-20 22:31:35 +0700

    ngerubah createReportForm.blade

--- resources/views/createReportForm.blade.php
+++ resources/views/createReportForm.blade.php
@@ -13,13 +13,13 @@ session_start();
 
 @section('content')
 <div id="createForm">
- <h1> Create Report Form </h1>
+ <h1 style="text-align: center"> Create Report Form </h1>
 </div>
 
 <br>
-<div class="col-md-8">
+<div class="col-md-12">
   <div class="table-responsive">
-  <table class="table" >  
+    <table class="table" style="margin-left:25%; margin-right:15%;">  
       <tbody>
         <tr>
           <td>Job vacancy</td>
@@ -36,6 +36,9 @@ session_start();
       </tbody>
     </table>
   </div>
+</div><br>
+
+<div class="col-md-12">
   <div class="table-responsive">
     <table class="table" style="margin-left:25%, margin-right:15%;">
       <h3>Competency List</h3>
@@ -43,56 +46,82 @@ session_start();
         <th>#</th>
         <th>Nama Kompetensi</th>
         <th>Penjelasan</th>
+        <th>Select</th>
       </thead>
       <tbody>
         <?php $i=0; ?>
-        @foreach($competency as $competency)
+        @foreach($competency as $comp)
         <?php $i++; ?>
-        <tr>
+        <tr class="competencyList">
           <td>{{ $i }}</td>
-          <td>  <div id="competencyList"><div id="{{$competency->id_kompetensi}}" class="kompetensi">{{$competency->nama_kompetensi}}</div></div></td>
-          <td>{{ $competency->penjelasan_kompetensi }}</td>
+          <td>  <div><div id="{{$comp->id_kompetensi}}" class="kompetensi">{{$comp->nama_kompetensi}}</div></div></td>
+          <td>{{ $comp->penjelasan_kompetensi }}</td>
+          <td>
+            <button id="{{$comp->id_kompetensi}}" class="opsi-kompetensi tambah-kompetensi active">add</button>
+            <button id="{{$comp->id_kompetensi}}" class="opsi-kompetensi hapus-kompetensi" style="display:none">remove</button>
+          </td>
+          <td></td>
         </tr>        
         @endforeach  
       </tbody>
     </table>
   </div>
-  <div id="choosen">
-    <form>
-      <div id="kompetensi-choose">
-      </div>
-      <input type="submit" value="Submit">
-    </form>
-  </div>
-
-  <div class="kompetensidiv">lala</div>
 </div>
 
+<div>
+  @foreach($competency as $com)
+  <div id = "{{$com->id_kompetensi}}" class = "choose" style = "display:none">{{$com->nama_kompetensi}}</div>
+  @endforeach
+  <button class="simpan">Submit</button>
+</div>  
 
 <script type="text/javascript">
-  $(document).ready(function() {
-    var array_id = new Array();
-    $('.kompetensi').click(function() {
-          // get kompetensi id
-          var id = $(this).attr('id');
-          var name = $(this).html();
-          var exist = false;
-          for (var i = array_id.length - 1; i >= 0; i--) {
-            if (array_id[i] == id) {
-              exist = true;
-            };
-          };
-          if (exist == false) {
-            array_id.push(id); 
-            $('#kompetensi-choose').append('<div id="'+id+'" class="kompetensidiv">'+name+'<br><input type="hidden" name="'+id+'" value="'+id+'"/></div>');
-          };
-        });
+$(document).ready(function() {
+  var array_id = new Array();
 
-    $('.kompetensidiv').click(function(){
-      alert('test');
-      $(this).remove();
-    });
-  });
+  $('.competencyList .opsi-kompetensi').click(function(){
+    $(this).hide();
+    $(this).siblings().show();
+
+    if($(this).hasClass('tambah-kompetensi')){
+                //console.log($(this).parent().parent().find('.kompetensi').html());
+                var id = $(this).attr('id');
+                $('.choose#'+id).show();
+                var exist = false;
+                for (var i = array_id.length - 1; i >= 0; i--) {
+                  if(array_id[i] == id){
+                    exist = true;
+                  }
+                };
+                if(exist==false){
+                  array_id.push(id);
+                }
+              }else if($(this).hasClass('hapus-kompetensi')){
+               // alert("khalila");
+               var id = $(this).attr('id');
+               $('.choose#'+id).hide();
+               for (var i = array_id.length - 1; i >= 0; i--) {
+                 if(array_id[i]==id){
+                      if(i==0){//kalo dia element pertama
+                        array_id.shift();
+                      }else if(i==array_id.length -1){//kalo dia element terakhir
+                        array_id.pop();
+                      }else{//element di tengah
+                        var part1 = array_id.slice(0,i);
+                        var part2 = array_id.slice(++i, array_id.length);
+                        array_id = part1.concat(part2); //menggabungkan array
+                      }
+                    }
+                  };  
+                }
+              });
+$('.simpan').click(function(){
+
+});
+});
+
+
+
 </script>
 
 @stop
